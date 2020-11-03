e1bf87e86b0f4f1f6898bf5cf2c4c0acb119850d
Martin McKeaveney <martin@budibase.com>
2020-11-03 22:35:53 +0000

    refactor

--- superfluous/src/main.rs
+++ superfluous/src/main.rs
@@ -1,23 +1,24 @@
 extern crate dirs;
 
-use std::{fs};
+use std::{fs, path};
 
 
 fn main() {
     println!("Hello, world!");
-    scan_files()
-}
-
-fn scan_files() {
     // TODO: update this to use pattern matching instead
     let home_dir = dirs::home_dir().expect("Failed reading home dir");
+    scan_files(&home_dir)
+}
 
-    println!("{}", home_dir.display());
+fn scan_files(dir: &path::PathBuf) {
 
-    for entry in fs::read_dir(home_dir).unwrap() {
+    for entry in fs::read_dir(dir).unwrap() {
         let path = entry.unwrap().path();
-        let metadata = fs::metadata(&path);
+        let metadata = fs::metadata(&path).unwrap();
 
-        println!("{}", metadata)
+        match metadata.is_dir() {
+            true => scan_files(&path),
+            false => println!("File {}: Size = {}", path.display(), metadata.len())
+        }
     }
 }
